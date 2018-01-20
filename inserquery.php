4a075d24bfa6ac63845e146e0fbe0c23f770eae9
kajalvekariya24 <35589094+kajalvekariya24@users.noreply.github.com>
2018-01-20 11:14:03 +0530

    Update inserquery.php

--- inserquery.php
+++ inserquery.php
@@ -27,3 +27,42 @@
 
      ?>
      </form>
+==================================================================================================
+<form action="" method="post"  enctype="multipart/form-data">
+  <label for="first-name-text">First name: </label><input type="text" id="first-name-text" name="firstNametxt" value="" /><br/>
+  <label for="last-name-text">Last name: </label><input type="text" id="last-name-text" name="lastNametxt" value="" /><br/>
+  <label for="email">Email: </label><input type="text" id="email" name="email" value="" /><br/>
+  <label for="query">Query: </label><input type="text" id="query" name="query" value="" /><br/>
+  <input name="Submit" type="submit" value="Submit">
+</form>
+<?php
+if( isset($_POST['submit']) ) {
+
+  //get posted value from the form
+  $firstName = $_POST["firstNametxt"];
+  $lastName = $_POST["lastNametxt"];
+  $email = $_POST["email"];
+  $query = $_POST["query"];
+
+  //deal with database in WordPress way
+  global $wpdb;
+  $contactus_table = $wpdb->prefix."contactus";
+  $wpdb->insert( 
+    $contactus_table, 
+    array( 
+        'firstname' => $firstName, 
+        'lastname'  => $lastName,
+        'email'     => $email,
+        'query'     => $query,
+        'reg_date'  => current_time( 'mysql' ) // http://codex.wordpress.org/Function_Reference/current_time
+    ), 
+    array( 
+      '%s', //data type is string
+      '%s',
+      '%s',
+      '%s',
+      '%s' 
+    ) 
+  );
+}
+?>
