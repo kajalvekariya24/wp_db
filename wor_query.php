189d84f1d1f307a8f586ad446113581f306e2cf9
kajalvekariya24 <35589094+kajalvekariya24@users.noreply.github.com>
2018-01-20 11:50:56 +0530

    Update wor_query.php

--- wor_query.php
+++ wor_query.php
@@ -1,1 +1,31 @@
 
+<?php
+global $wpdb;
+$customers = $wpdb->get_results("SELECT * FROM customers;");
+print_r($customers);
+?>
+
+========================================================================
+<?php
+
+if (is_user_logged_in()):
+
+global $wpdb;
+$customers = $wpdb->get_results("SELECT * FROM customers;");
+
+echo "<table>";
+foreach($customers as $customer){
+echo "<tr>";
+echo "<td>".$customer->name."</td>";
+echo "<td>".$customer->email."</td>";
+echo "<td>".$customer->phone."</td>";
+echo "<td>".$customer->address."</td>";
+echo "</tr>";
+}
+echo "</table>";
+else:
+echo "Sorry, only registered users can view this information";
+endif;
+
+?>
+============================================================================
