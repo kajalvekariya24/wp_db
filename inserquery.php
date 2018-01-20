36f0ca6ba9b518a8e1a7b53944e6b0d445be91d1
kajalvekariya24 <35589094+kajalvekariya24@users.noreply.github.com>
2018-01-20 11:10:08 +0530

    Update inserquery.php

--- inserquery.php
+++ inserquery.php
@@ -1,1 +1,29 @@
 
+<form action="" method="post">
+          FirstName <input type="text" name="firstNametxt" value="" /><br/>
+          LastName   <input type="text" name="lastNametxt" value="" /><br/>
+          email <input type="text" name="email" value="" /><br/>
+          Query <input type="text" name="query" value="" /><br/>
+          <input name="Submit" type="submit" value="Submit">
+      </form>
+      <form method="post">
+          <?php   
+          global $wpdb;
+                  $firstName = $_POST["firstNametxt"];
+                  $lastName = $_POST["lastNametxt"];
+                  $email = $_POST["email"];
+                  $query = $_POST["query"];
+
+                  echo $firstName;
+          $contactus_table = $wpdb->prefix."contactus";
+
+          $sql = "INSERT INTO $contactus_table (id, firstname, lastname, email,                                               
+
+            query, reg_date) VALUES ('2', $firstName, $lastName, $email, $query,
+
+            CURRENT_TIMESTAMP);";                  
+
+            $wpdb->query($sql)) 
+
+     ?>
+     </form>
