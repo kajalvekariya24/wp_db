a9911e0263ccf5af611052e5a2b2a3b7c70131a1
kajalvekariya24 <35589094+kajalvekariya24@users.noreply.github.com>
2018-01-20 15:12:31 +0530

    Update wor_query.php

--- wor_query.php
+++ wor_query.php
@@ -29,3 +29,18 @@ endif;
 
 ?>
 ============================================================================
+<?php
+function perform_database_action(){
+    mysql_query(“INSERT into table_name (col1, col2, col3) VALUES ('$value1','$value2', '$value3');
+}
+           php to wordpress convert
+            --------------------
+                
+                
+ <?php
+function perform_database_action(){
+    global $wpdb;
+    $data= array('col1'=>$value1,'col2'=>$value2,'col3'=>$value3);
+    $format = array('%s','%s','%s');
+    $wpdb->insert('table_name', $data, $format);
+}
